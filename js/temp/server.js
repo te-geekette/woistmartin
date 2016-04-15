5cdff423b8b298459db1c226cc8d32638eea16e7
NavaL <ayoyo@thoughtworks.com>
2016-04-15 17:55:26 +0200

    Sree + Ayoyo | faking and making api calls to get the long and lat of Martin

--- js/temp/server.js
+++ js/temp/server.js
@@ -3,9 +3,15 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.delete('/martin-url/lat', function(req, res) {
-  res.send(53.14677033085084);
+  res.send(200, "53.14677033085084");
 });
 
 app.delete('/martin-url/lng', function(req, res) {
-  res.send(27.2021484375);
+  res.send(200, "27.2021484375");
+});
+
+app.use('/', express.static('../../'));
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
